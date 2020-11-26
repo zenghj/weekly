f55b11e911dedb0e7e741ab343a77e365000380f
zenghj <zenghj@outlook.com>
2020-11-26 16:13:33 +0800

    Update weekly item

--- scripts/weekly.js
+++ scripts/weekly.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const fs = require('fs')
-const date = new Date()
+const date = new Date('2020/11/23')
 const readmeFilePath = path.resolve(__dirname, '../README.md')
 const { fsPromises } = require('./utils')
 
